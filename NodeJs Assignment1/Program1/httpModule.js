30c21576cdf27d11dbf0a4f6f282427be9313555
dolapo-mosuro <mosurodolapo@gmail.com>
2023-08-08 13:50:04 +0100

    HTTP Module

--- NodeJs Assignment1/Program1/httpModule.js
+++ NodeJs Assignment1/Program1/httpModule.js
@@ -1,17 +1,16 @@
 const http = require("http");
 
 const PORT = 3000;
+const HOSTNAME = "localhost";
 
-// Create an http server
-const server = http.createServer((req, res) => {
-	// Set the response header
-	res.writeHead(200, { "Content-Type": "text/plain" });
-});
+function requestHandler(req, res) {
+	console.log(req);
+	res.end("Hello world");
+}
 
-// Return “Hello world” from the response
-res.end("Hello, World!\n");
-// Ensure when you navigate to the server on the browser, “Hello world” is returned.
+// Create the server
+const server = http.createServer(requestHandler);
 
-server.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
+server.listen(PORT, HOSTNAME, () => {
+	console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
